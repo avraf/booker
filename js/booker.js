0c9f3c62e897f5ea910219003789f6e30349b46b
avraf <avrahamraf@gmail.com>
2015-09-09 02:20:43 +0300

    missed file

---
+++ js/booker.js
@@ -0,0 +1,18 @@
+var bookerComponent = {
+    controller: function() {},
+    view: function() {}
+}
+
+
+var todo = {};
+
+//for simplicity, we use this component to namespace the model classes
+
+//the Todo class has two properties
+todo.Todo = function(data) {
+    this.description = m.prop(data.description);
+    this.done = m.prop(false);
+};
+
+//the TodoList class is a list of Todo's
+todo.TodoList = Array;
